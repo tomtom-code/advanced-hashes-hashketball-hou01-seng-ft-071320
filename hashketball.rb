b4327f39e916d10cba165621100ea5d565b112c1
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-08 06:46:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,16 +131,16 @@ end
 
 def num_points_scored(player_name)
   
-  player_scored = 0
+  player_scored = []
    game_hash.each do |key1, value1|
     value1[:players].each do |key2, value2|
       if key2[:player_name] == player_name
         player_scored = key2[:points]
-        binding.pry
+        
       end
     end
   end
-   
+   binding.pry
 end
 
 
