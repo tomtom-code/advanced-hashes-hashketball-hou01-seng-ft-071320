4d56adac97c9c8ae35164313a484aa42e74975e2
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-08 08:41:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -188,7 +188,7 @@ def player_numbers(team_name)
   
   if game_hash[:home][:team_name] == team_name
     game_hash[:home][:players].each do |key1, value1|
-     return numbers<< key1[:number]
+      numbers<< key1[:number]
      binding.pry
       
     end
