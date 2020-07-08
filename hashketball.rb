f9aaceee2d6cc2eaf16d0ba6f2ffe975b67e2ce0
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-08 06:00:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,8 +131,10 @@ end
 
 def num_points_scored(player_name)
   
-  player_scored = 0
-  game_hash[:pl]
+  home_player_scored = 0
+  away_player_scored = 0
+  
+  if game_hash[:home][:players]
   #binding.pry
   
 end
