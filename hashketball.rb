14a2f18a1d12d05e60c0253d87367d6c4bd1432d
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-08 08:18:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -180,12 +180,14 @@ end
 
 
 def player_numbers(team_name)
-  binding.pry
+  
   #Build a method, `player_numbers`, that takes in an argument of a team name and
   #returns an `Array` of the jersey numbers for that team.
   
   if game_hash[:home][:team_name] == team_name
-    game_hash[:home][:players].map {|players| 
+    game_hash[:home][:players].map {|player| player[:number]}
+    binding.pry
+  end
     
 
   
